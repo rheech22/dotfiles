8c36cbf4ce6eef5cf39335b76a06e05285db9caf
Aiden <rheech22@gmail.com>
2023-12-24 01:46:03 +0900

    map keys for toggles

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,10 @@
 return {
   {
     "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    keys = {
+      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "git diff view open" },
+      { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "git diff view close" },
+    },
   },
 }
