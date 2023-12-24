099301b444b042d9bc8a6ea5945b48d293dc924a
Aiden <rheech22@gmail.com>
2023-12-25 02:23:24 +0900

    * delete markdown syntax in vimwiki list

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -4,8 +4,8 @@ return {
     branch = "dev",
     init = function()
       vim.g.vimwiki_list = {
-        { path = "~/Projects/rheech22.github.io/src/wikis", syntax = "markdown", ext = ".md" },
-        { path = "~/Documents/gdrive", syntax = "markdown", ext = ".md" },
+        { path = "~/Projects/rheech22.github.io/src/wikis", ext = ".md" },
+        { path = "~/Documents/gdrive", ext = ".md" },
       }
       vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown" }
     end,
