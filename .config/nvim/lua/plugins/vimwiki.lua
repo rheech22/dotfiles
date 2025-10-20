409639e3a88fe77ad3459bbd962ace0c4175022c
Aiden <rheech22@gmail.com>
2025-10-20 10:51:35 +0900

    vimwiki

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -2,9 +2,9 @@ return {
   config = function()
     vim.g.vimwiki_list = {
       {
-        path = vim.fn.expand '~/Library/Mobile Documents/com~apple~CloudDocs/Notes',
+        path = '~/Dropbox/wiki',
         ext = '.md',
-        diary_rel_path = 'retrospectives',
+        diary_rel_path = 'diary',
       },
     }
     vim.g.vimwiki_ext2syntax = {
@@ -12,13 +12,11 @@ return {
       ['.mdx'] = 'markdown',
     }
 
-    -- autocmds
-    local group = vim.api.nvim_create_augroup('VimwikiFrontmatter', { clear = true })
+    -- autocmds      local group =
     vim.api.nvim_create_autocmd('BufNewFile', {
-      group = group,
+      group = vim.api.nvim_create_augroup('diary-date', { clear = true }),
       pattern = {
-        '*/note/*.md',
-        '*/blog/*.md',
+        '*/diary/*.md',
       },
       callback = function()
         if vim.fn.line '$' > 1 then
@@ -26,12 +24,7 @@ return {
         end
 
         local template = {
-          '---',
-          'created: ' .. os.date '%Y-%m-%d %H:%M:%S +0900',
-          'updated: ' .. os.date '%Y-%m-%d %H:%M:%S +0900',
-          'tags: []',
-          '---',
-          '',
+          '# ' .. os.date '%Y-%m-%d',
         }
 
         vim.api.nvim_buf_set_lines(0, 0, 0, false, template)
@@ -39,35 +32,7 @@ return {
         vim.fn.execute 'normal! G'
         vim.fn.execute 'normal! $'
 
-        print 'frontmatter added.'
-      end,
-    })
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = group,
-      pattern = {
-        '*/note/*.md',
-        '*/blog/*.md',
-      },
-      callback = function()
-        if vim.g.md_modify_disabled then
-          return
-        end
-        if not vim.bo.modified then
-          return
-        end
-
-        local save_cursor = vim.fn.getpos '.'
-        local n = math.min(10, vim.fn.line '$')
-
-        for i = 1, n do
-          local line = vim.fn.getline(i)
-          if line:match '^updated: ' then
-            vim.fn.setline(i, 'updated: ' .. os.date '%Y-%m-%d %H:%M:%S +0900')
-            vim.fn.setpos('.', save_cursor)
-            return
-          end
-        end
+        print 'title added.'
       end,
     })
   end,
