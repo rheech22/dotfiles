93cdd82a3c39a31cd2ebd0f2305db7a1304eafae
Aiden <rheech22@gmail.com>
2026-01-10 23:01:42 +0900

    style markdown preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,9 @@
 return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  ft = { 'markdown' },
   config = function()
-    vim.g.mkdp_markdown_css = '~/.config/nvim/md_preview.css'
+    vim.g.mkdp_markdown_css = vim.fn.expand '~/.config/nvim/md_preview.css'
   end,
 }
