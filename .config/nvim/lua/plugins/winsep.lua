7707ac4b4a94b7092c4fd4bbb62b0dd5077b511f
Aiden <rheech22@gmail.com>
2026-01-11 12:33:51 +0900

    opts winsep

--- .config/nvim/lua/plugins/winsep.lua
+++ .config/nvim/lua/plugins/winsep.lua
@@ -1,5 +1,15 @@
 return {
   config = function()
-    require('colorful-winsep').setup {}
+    require('colorful-winsep').setup {
+      -- https://github.com/nvim-zh/colorful-winsep.nvim
+      border = 'double',
+      excluded_ft = { 'mason' },
+      animate = {
+        -- NOTE: progressive option doesn't work well, check below
+        -- https://github.com/nvim-zh/colorful-winsep.nvim/issues/107
+        enabled = 'shift',
+      },
+    }
+    vim.api.nvim_set_hl(0, 'ColorfulWinSep', { fg = '#e8b589', bg = 'black' })
   end,
 }
